cf56adb410fea4f85f8bb1591db21b2bf9dff554
JT <jakubtalacko@gmail.com>
2019-11-02 15:02:10 +0100

    user comment seeder

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -6,7 +6,10 @@ use App\Comment;
 use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
+
+
     return [
+        'user_id' => factory(App\User::class),
         'user_id' => $faker->numberBetween(1, 1000),
         'event_id' => $faker->numberBetween(1, 1000),
         'description' => $faker->sentence(6, true)
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(App\User::class, 10)->create();
+
+        factory(App\User::class, 50)->create()->each(function ($user) {
+            $user->comments()->save(factory(App\Comment::class)->make());
+        });
+    }
+}
