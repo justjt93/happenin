941b22f372f44bbecda313806cab50f178eac00a
JT <jakubtalacko@gmail.com>
2019-11-02 15:53:13 +0100

    seeders done

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Event;
+use Faker\Generator as Faker;
+
+$factory->define(Event::class, function (Faker $faker) {
+    return [
+        'title' => $faker->cityPrefix,
+        'description' => $faker->sentence(12, true),
+        'latitude' => $faker->latitude(50.059148, 50.112787),
+        'longitude' => $faker->longitude(14.356330, 14.467470),
+        'address' => $faker->address,
+        'starts_at' => $faker->dateTimeBetween('now', '+1 days'),
+        'ends_at' => $faker->dateTimeBetween('+2 days', '+6 days'),
+        'type_id' => $faker->numberBetween(1, 5),
+        'user_id' => $faker->numberBetween(1, 50)
+
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(CommentSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(TypeSeeder::class);
+        $this->call(EventSeeder::class);
     }
 }
