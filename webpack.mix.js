1715375b66e49ed4f8ff528efe0e159a66d45416
Vojtech Vanek <vanevo00@gmail.com>
2019-11-02 12:44:01 +0100

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,11 @@
-const mix = require('laravel-mix');
-
+const mix = require("laravel-mix");
+require("dotenv").config();
 /*
- |--------------------------------------------------------------------------
+ |-----------------------------------------------------------------------</section><section class="
+ noflag">
  | Mix Asset Management
- |--------------------------------------------------------------------------
+ |-----------------------------------------------------------------------</section><section class="
+ noflag">
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
@@ -11,5 +13,26 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.options({
+    processCssUrls: false
+});
+
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: "source-map"
+    }).sourceMaps();
+}
+
+mix.react("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+
+    .browserSync({
+        host: "localhost",
+        port: 3000,
+        proxy: {
+            target: process.env.APP_URL // Yay! Using APP_URL from the .env file!
+        }
+    });
+
+// add versioning
+mix.version();
