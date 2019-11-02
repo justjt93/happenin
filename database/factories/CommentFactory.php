6e9d41c5591fb0db9720d63a9f40252067d3cb56
JT <jakubtalacko@gmail.com>
2019-11-02 14:33:57 +0100

    seeder comments

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->numberBetween(1, 1000),
+        'event_id' => $faker->numberBetween(1, 1000),
+        'description' => $faker->sentence(6, true)
+    ];
+});
