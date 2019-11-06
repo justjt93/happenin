b5f3755512cae929c9ce27bf6c1d51654d234071
Vojtech Vanek <vanevo00@gmail.com>
2019-11-06 17:58:26 +0100

    menu styling done

--- resources/js/App/Components/Menu.jsx
+++ resources/js/App/Components/Menu.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 
 export default class Menu extends React.Component {
 
+    openNav = () => {
+        document.getElementById("sidenav").style.width = "250px";
+        document.getElementById("burger").style.display = "none";
+    }
+
+    closeNav = () => {
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("burger").style.display = "flex";
+    } 
+    
     render() {
         return (
             <>
                 <div className="menu">
                     <h2>happenin'</h2>
-                    <img src="./images/burger.png" alt="menu"/>
+                    <div className="burger" id="burger" onClick={this.openNav}>
+                        <div className="line"></div>
+                        <div className="line"></div>
+                        <div className="line"></div>
+                    </div>
+                </div>
+                <div className="sidenav" id="sidenav">
+                    <div className="close-sidenav" onClick={this.closeNav}>&times;</div>
+                    <a href="#"><div className="sidenav-item">Login</div></a>
+                    <a href="#"><div className="sidenav-item">Test</div></a>
+                    <a href="#"><div className="sidenav-item">Test</div></a>
+                    <a href="#"><div className="sidenav-item">Test</div></a>
                 </div>
             </>
         )
