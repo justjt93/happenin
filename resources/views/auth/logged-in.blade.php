b1bbd4301283f91d97eb02ec19f4619ccde73e86
Vojtech Vanek <vanevo00@gmail.com>
2019-11-07 23:25:26 +0100

    blades refactored

--- resources/views/auth/logged-in.blade.php
+++ resources/views/auth/logged-in.blade.php
@@ -1,4 +1,4 @@
-@extends('auth.layout')
+@extends('layout')
 
 @section('title')
 Logged in
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('auth.layout')
+@extends('layout')
 
 @section('title')
 Registration 
@@ -9,42 +9,5 @@ Registration
     <div id="register"></div>
 
     <script src="{{ mix('js/register.js') }}"></script>
-
-  {{-- <div class="register-form">
-      <h3>Registration</h3>
- 
-      <form action="{{ route('register') }}" method="POST">
-      @csrf
-    
-      <div class="form-group">
-          <input class="form-control @error('name')is-invalid @enderror" type="text" name="name" id="name" placeholder="Your name" value="{{ old('name') }}">
-          @error('name')
-                <span class="error-message">{{ $message }}</span>
-          @enderror
-      </div>
-    
-      <div class="form-group">
-          <input class="form-control @error('name')is-invalid @enderror" id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
-          @error('email')
-                <span class="error-message">{{ $message }}</span>
-          @enderror
-      </div>
-    
-      <div class="form-group">
-          <input class="form-control @error('name')is-invalid @enderror" id="password" type="password" name="password" placeholder="Password" value="">
-          @error('password')
-                <span class="error-message">{{ $message }}</span>
-          @enderror
-      </div>
-    
-      <div class="form-group">
-          <input class="form-control" type="password" name="password_confirmation" id="password_confirmation" placeholder="Re-enter password" value="">
-      </div>
-    
-      <button type="submit" class="btn-sign-up">Sign up</button>
-    
-      </form>
-  </div> --}}
-  
   
 @endsection
