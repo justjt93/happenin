1a99ee1f8b8bdfe9c46aaf463f1141efd3bf4f09
itana <itanabulatovic.ib@gmail.com>
2019-11-13 22:03:32 +0100

    category btns

--- resources/js/App/Event/AddEventForm.jsx
+++ resources/js/App/Event/AddEventForm.jsx
@@ -8,11 +8,19 @@ export default class AddEventForm extends React.Component {
     this.state = {
         title: "",
         address: "",
-        starts_at: null,
-        ends_at: null,
+        starts_at: "2019-09-11T19:20",
+        ends_at: "2019-09-11T19:20",
         description:"",
-        type_id: null,
+        type_id: "",
     };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleStartTimeChange = this.handleStartTimeChange.bind(this);
+    this.handleEndTimeChange = this.handleEndTimeChange.bind(this);
+    this.handleCategorySelection = this.handleCategorySelection.bind(this);
+     
+
   }
 
   handleNameChange = (event) => {
@@ -42,6 +50,12 @@ export default class AddEventForm extends React.Component {
   handleEndTimeChange = (event) => {
     this.setState({
       ends_at: event.target.value,
+    })
+  }
+
+  handleCategorySelection = (event) => {
+    this.setState({
+      type_id:event.target.value,
     })
   }
 
@@ -94,8 +108,8 @@ export default class AddEventForm extends React.Component {
             </div>
 
             <div className="form-group">
-              <label htmlFor="ends_atme">Ends at: </label><br/>
-              <input type="datetime-local" value="2019-09-11T19:20"onChange={this.handleEndTimeChange}/> 
+              <label htmlFor="ends_at">Ends at: </label><br/>
+              <input type="datetime-local" name="ends_at" value="2019-09-11T19:20" onChange={this.handleEndTimeChange}/> 
             </div>
 
             <div className="form-group">
@@ -108,24 +122,19 @@ export default class AddEventForm extends React.Component {
             <label htmlFor="type_id">Choose categroy: </label><br/>
 
               <div className="radio-btns">
-                <input type="radio" id="control_01" name="type_id" value="1" onClick={(event) => {
-                this.setState({type_id: event.target.value})}} />
+                <input type="radio" id="control_01" name="type_id" value='1' checked={this.state.type_id === "1"} onChange={this.handleCategorySelection} />
                 <label htmlFor="control_01">art</label>
 
-                <input type="radio" id="control_02" name="type_id" value="2"  onClick={(event) => {
-                this.setState({type_id: event.target.value})}}/>
+                <input type="radio" id="control_02" name="type_id" value="2"  checked={this.state.type_id === "2"} onChange={this.handleCategorySelection}/>
                 <label htmlFor="control_02">music</label>
 
-                <input type="radio" id="control_03" name="type_id" value="3"  onClick={(event) => {
-                this.setState({type_id: event.target.value})}}/>
+                <input type="radio" id="control_03" name="type_id" value="3"  checked={this.state.type_id === "3"} onChange={this.handleCategorySelection}/>
                 <label htmlFor="control_03" defaultChecked >sport</label>
 
-                <input type="radio" id="control_04" name="type_id" value="4"  onClick={(event) => {
-                this.setState({type_id: event.target.value})}}/>
+                <input type="radio" id="control_04" name="type_id" value="4"  checked={this.state.type_id === "4"} onChange={this.handleCategorySelection}/>
                 <label htmlFor="control_04">chill</label>
 
-                <input type="radio" id="control_05" name="type_id" value="5"  onClick={(event) => {
-                this.setState({type_id: event.target.value})}}/>
+                <input type="radio" id="control_05" name="type_id" value="5"  checked={this.state.type_id === "5"} onChange={this.handleCategorySelection}/>
                 <label htmlFor="control_04">social</label> 
               </div>       
               
