5200428a0704ff10bdc17167ac15b92b74866514
Vojtech Vanek <vanevo00@gmail.com>
2019-11-13 23:48:42 +0100

    user edit and info pure and hooks

--- resources/js/App/User/UserEdit.jsx
+++ resources/js/App/User/UserEdit.jsx
@@ -1,110 +1,88 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Menu from "../Components/Menu.jsx";
 import Footer from "../Components/Footer.jsx";
 
-export default class UserEdit extends React.Component {
-  constructor(props) {
-    super(props);
+ const UserEdit = () => {
+    const [name, setName] = useState('Loading..'); 
+    const [email, setEmail] = useState('Loading..');
+    const [password, setPassword] = useState();
+    const [newPassword, setNewPassword] = useState();
+    const [confirmNewPassword, setConfirmNewPassword] = useState();
+    const [id, setId] = useState();
+    const [data, setData] = useState();
 
-    this.state = {
-        name: "loading..",
-        email: "loading..",
-        password: null,
-        newPassword: null,
-        confirmNewPassword: null,
-        id: null
-    };
-  }
+    useEffect(() => {
+        const user = (JSON.parse(document.querySelector('meta[name="logged-in-user"]').getAttribute('content')));
 
-  componentDidMount() {
-    const user = (JSON.parse(document.querySelector('meta[name="logged-in-user"]').getAttribute('content')));
+        setName(user.name)
+        setEmail(user.email)
+        setId(user.id)
+      }, []);
 
-    this.setState({
-      name: user.name,
-      email: user.email,
-      id: user.id
-    })
-  }
+    const handleNameChange = (event) => {
+        setName(event.target.value)
+    }
 
-  handleNameChange = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
 
-  handleEmailChange = (event) => {
-    this.setState({
-      email: event.target.value
-    })
-  }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
 
-  handlePasswordChange = (event) => {
-    this.setState({
-      password: event.target.value
-    })
-  }
+    const handleNewPasswordChange = (event) => {
+        setNewPassword(event.target.value)
+    }
 
-  handleNewPasswordChange = (event) => {
-    this.setState({
-      newPassword: event.target.value
-    })
-  }
+    const handleConfirmNewPasswordChange = (event) => {
+        setConfirmNewPassword(event.target.value)
+    }
 
-  handleConfirmNewPasswordChange = (event) => {
-    this.setState({
-      confirmNewPassword: event.target.value
-    })
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        fetch(`/userdetail/edit/${id}`, {
+        method: 'POST',
+        headers: {
+            'Accept':       'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+            newPassword: newPassword,
+            newPassword_confirmation: confirmNewPassword,
+        })
+        })
+        .then (response => response.json())
+        .then(data => setData(data))
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+    const handleChangePasswordSubmit = (event) => {
+        event.preventDefault();
+        
+        fetch(`/userdetail/passwordchange/${id}`, {
+        method: 'POST',
+        headers: {
+            'Accept':       'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify({
+            password: password,
+            newPassword: newPassword,
+            newPassword_confirmation: confirmNewPassword,
+        })
+        })
+        .then (response => response.json())
+        .then(data => setData(data))
+    }
     
-    fetch(`/userdetail/edit/${this.state.id}`, {
-      method: 'POST',
-      headers: {
-          'Accept':       'application/json',
-          'Content-Type': 'application/json',
-          'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-      },
-      body: JSON.stringify({
-          name: this.state.name,
-          email: this.state.email,
-          password: this.state.password,
-          newPassword: this.state.newPassword,
-          newPassword_confirmation: this.state.confirmNewPassword,
-      })
-    })
-    .then (response => response.json())
-    .then(data => this.setState ({
-      response: data
-    }));
-  }
-
-  handleChangePasswordSubmit = (event) => {
-    event.preventDefault();
-    
-    fetch(`/userdetail/passwordchange/${this.state.id}`, {
-      method: 'POST',
-      headers: {
-          'Accept':       'application/json',
-          'Content-Type': 'application/json',
-          'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-      },
-      body: JSON.stringify({
-          password: this.state.password,
-          newPassword: this.state.newPassword,
-          newPassword_confirmation: this.state.confirmNewPassword,
-      })
-    })
-    .then (response => response.json())
-    .then(data => this.setState ({
-      response: data
-    }));
-  }
-    
-    render() {
-      let errors = this.state.response ? this.state.response.errors ? this.state.response.errors: "" : "";
-      let status = this.state.response ? this.state.response.status ? this.state.response.status: "" : "";
+    let errors = data ? data.errors ? data.errors: "" : "";
+    let status = data ? data.status ? data.status: "" : "";
 
 
       return (
@@ -114,27 +92,27 @@ export default class UserEdit extends React.Component {
               <div className="user-item">
               <p>change user information</p><br/>
               <p className="complete-message">{status}</p>
-              <form action="" onSubmit={this.handleSubmit}>
+              <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="name">name</label>
-                <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleNameChange}/><br/> 
+                <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/><br/> 
                 <span className="error-message">{errors.name}</span><br/>
 
                 <label htmlFor="email">email</label>
-                <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleEmailChange}/><br/> 
+                <input type="text" id="email" name="email" value={email} onChange={handleEmailChange}/><br/> 
                 <span className="error-message">{errors.email}</span><br/>
 
                 <input type="submit" value="Submit changes"/>
               </form>
 
-              <form action="" onSubmit={this.handleChangePasswordSubmit}>
+              <form action="" onSubmit={handleChangePasswordSubmit}>
                 <p>change password</p>
-                  <input type="password" id="password" name="password" placeholder="old password" onChange={this.handlePasswordChange}/><br/> 
+                  <input type="password" id="password" name="password" placeholder="old password" onChange={handlePasswordChange}/><br/> 
                   <span className="error-message">{errors.password}</span><br/>
 
-                  <input type="password" id="newPassword" name="newPassword" placeholder="new password" onChange={this.handleNewPasswordChange}/><br/> 
+                  <input type="password" id="newPassword" name="newPassword" placeholder="new password" onChange={handleNewPasswordChange}/><br/> 
                   <span className="error-message">{errors.newPassword}</span><br/>
 
-                  <input type="password" id="newPassword_confirmation" name="newPassword_confirmation" placeholder="confirm new password" onChange={this.handleConfirmNewPasswordChange}/><br/>
+                  <input type="password" id="newPassword_confirmation" name="newPassword_confirmation" placeholder="confirm new password" onChange={handleConfirmNewPasswordChange}/><br/>
 
                   <input type="submit" value="Change password"/> 
               </form>
@@ -143,5 +121,6 @@ export default class UserEdit extends React.Component {
             </div>
           </>
         )
-    }
 }
+
+export default UserEdit
