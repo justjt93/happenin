2449296745a2e69186b1df528d358459fd824fe4
Vojtech Vanek <vanevo00@gmail.com>
2019-11-14 17:55:13 +0100

    state and form handling improved

--- resources/js/App/User/UserEdit.jsx
+++ resources/js/App/User/UserEdit.jsx
@@ -3,46 +3,31 @@ import Menu from "../Components/Menu.jsx";
 import Footer from "../Components/Footer.jsx";
 
  const UserEdit = () => {
-    const [name, setName] = useState('Loading..'); 
-    const [email, setEmail] = useState('Loading..');
-    const [password, setPassword] = useState();
-    const [newPassword, setNewPassword] = useState();
-    const [confirmNewPassword, setConfirmNewPassword] = useState();
-    const [id, setId] = useState();
+    const [formInputValues, setFormInputValues] = useState({ name: 'Loading..', email: 'Loading..', password: null, newPassword: null, confirmNewPassword: null, id: null});
     const [data, setData] = useState();
 
     useEffect(() => {
         const user = (JSON.parse(document.querySelector('meta[name="logged-in-user"]').getAttribute('content')));
 
-        setName(user.name)
-        setEmail(user.email)
-        setId(user.id)
+        setFormInputValues({
+            ...formInputValues,
+            name: user.name,
+            email: user.email,
+            id: user.id
+        })
       }, []);
 
-    const handleNameChange = (event) => {
-        setName(event.target.value)
-    }
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value)
-    }
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value)
-    }
-
-    const handleNewPasswordChange = (event) => {
-        setNewPassword(event.target.value)
-    }
-
-    const handleConfirmNewPasswordChange = (event) => {
-        setConfirmNewPassword(event.target.value)
-    }
+      const handleTextValueChange = e => {
+        setFormInputValues({
+          ...formInputValues,
+          [e.target.id]: e.target.value
+        })
+      };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         
-        fetch(`/userdetail/edit/${id}`, {
+        fetch(`/userdetail/edit/${formInputValues.id}`, {
         method: 'POST',
         headers: {
             'Accept':       'application/json',
@@ -50,11 +35,8 @@ import Footer from "../Components/Footer.jsx";
             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
         },
         body: JSON.stringify({
-            name: name,
-            email: email,
-            password: password,
-            newPassword: newPassword,
-            newPassword_confirmation: confirmNewPassword,
+            name: formInputValues.name,
+            email: formInputValues.email,
         })
         })
         .then (response => response.json())
@@ -64,7 +46,7 @@ import Footer from "../Components/Footer.jsx";
     const handleChangePasswordSubmit = (event) => {
         event.preventDefault();
         
-        fetch(`/userdetail/passwordchange/${id}`, {
+        fetch(`/userdetail/passwordchange/${formInputValues.id}`, {
         method: 'POST',
         headers: {
             'Accept':       'application/json',
@@ -72,9 +54,9 @@ import Footer from "../Components/Footer.jsx";
             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
         },
         body: JSON.stringify({
-            password: password,
-            newPassword: newPassword,
-            newPassword_confirmation: confirmNewPassword,
+            password: formInputValues.password,
+            newPassword: formInputValues.newPassword,
+            newPassword_confirmation: formInputValues.confirmNewPassword,
         })
         })
         .then (response => response.json())
@@ -94,11 +76,11 @@ import Footer from "../Components/Footer.jsx";
               <p className="complete-message">{status}</p>
               <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="name">name</label>
-                <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/><br/> 
+                <input type="text" id="name" name="name" value={formInputValues.name} onChange={handleTextValueChange}/><br/> 
                 <span className="error-message">{errors.name}</span><br/>
 
                 <label htmlFor="email">email</label>
-                <input type="text" id="email" name="email" value={email} onChange={handleEmailChange}/><br/> 
+                <input type="text" id="email" name="email" value={formInputValues.email} onChange={handleTextValueChange}/><br/> 
                 <span className="error-message">{errors.email}</span><br/>
 
                 <input type="submit" value="Submit changes"/>
@@ -106,13 +88,13 @@ import Footer from "../Components/Footer.jsx";
 
               <form action="" onSubmit={handleChangePasswordSubmit}>
                 <p>change password</p>
-                  <input type="password" id="password" name="password" placeholder="old password" onChange={handlePasswordChange}/><br/> 
+                  <input type="password" id="password" name="password" placeholder="old password" onChange={handleTextValueChange}/><br/> 
                   <span className="error-message">{errors.password}</span><br/>
 
-                  <input type="password" id="newPassword" name="newPassword" placeholder="new password" onChange={handleNewPasswordChange}/><br/> 
+                  <input type="password" id="newPassword" name="newPassword" placeholder="new password" onChange={handleTextValueChange}/><br/> 
                   <span className="error-message">{errors.newPassword}</span><br/>
 
-                  <input type="password" id="newPassword_confirmation" name="newPassword_confirmation" placeholder="confirm new password" onChange={handleConfirmNewPasswordChange}/><br/>
+                  <input type="password" id="confirmNewPassword" name="newPassword_confirmation" placeholder="confirm new password" onChange={handleTextValueChange}/><br/>
 
                   <input type="submit" value="Change password"/> 
               </form>
