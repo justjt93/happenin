a34041306505506c7340955fa5e44f65dabb5e44
itana <itanabulatovic.ib@gmail.com>
2019-11-21 15:04:07 +0100

    responsive

--- resources/js/App/Components/App.jsx
+++ resources/js/App/Components/App.jsx
@@ -3,7 +3,7 @@ import Menu from "./Menu.jsx";
 import Map from "./Map.jsx";
 import Footer from "./Footer.jsx";
 import AddBtn from "./AddBtn.jsx";
-import EventList from "../Event/Eventlist.jsx";
+import EventList from "../Event/EventList.jsx";
 
 const App = () => {
     return (
