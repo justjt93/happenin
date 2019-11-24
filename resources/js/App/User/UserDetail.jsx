a3be1a32a5cfac1096c59f3313e2c305c72705b2
itana <itanabulatovic.ib@gmail.com>
2019-11-24 20:44:08 +0100

    personal profile page

--- resources/js/App/User/UserDetail.jsx
+++ resources/js/App/User/UserDetail.jsx
@@ -19,34 +19,42 @@ const UserDetail = () => {
           <>
             <div className="user-detail">
               <Menu />
-              <div className="user-item">
+              <div className="user-profile">
+
+              <h3>Your Profile Info</h3>
+
                 <div className="amazing-avatar">
                   <img src={user.avatar}></img>
                 </div>
 
                 <div className="personal-info">
-                  
-                  <h3>Your Profile Info</h3>
                   <div>
                     <p><strong>username:</strong> {user.name}</p>
                     <p><strong>email:</strong> {user.email}</p>
                     <p><strong>member since:</strong> {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`}</p>
                   </div>
-
                 </div>
+
                 <Router history={history}>
+                  
                   <div className="profile-button">
                     <Link to="/userdetail/editinfo"><Button color="info">Edit Info</Button></Link>
                     <Link to="/userdetail/editpassword"><Button color="info">Change Password</Button></Link>
                   </div>
+
                   <Route exact path='/userdetail/editinfo' component={InfoEdit} />
                   <Route exact path='/userdetail/editpassword' component={PasswordEdit} />
                 </Router>
-                <UserEvents
-                user={user}
-                userEvents={userEvents}
-                />
+
               </div>
+
+                <div className="profile-comments">
+                  <UserEvents
+                  user={user}
+                  userEvents={userEvents}
+                  />
+                </div>
+              
               <Footer />
             </div>
           </>
