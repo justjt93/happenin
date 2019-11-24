6b0009af7f4394e2b0c225f5b10dc622d58684f2
itana <itanabulatovic.ib@gmail.com>
2019-11-24 15:37:35 +0100

    adding social media avatar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,6 +11,7 @@ Registration
     <script type='text/javascript'>
         window.__username = '{{$name ?? ''}}' || null 
         window.__useremail = '{{$email ?? ''}}' || null
+        window.__avatar = '{{$avatar ?? ''}}' || null
     </script>
     <script src="{{ mix('js/register.js') }}"></script>
   
