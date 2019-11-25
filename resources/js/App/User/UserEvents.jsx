6df7d4ef899a70d85533aad43e2b6989758e57ee
itana <itanabulatovic.ib@gmail.com>
2019-11-25 13:37:11 +0100

    my-profile-events

--- resources/js/App/User/UserEvents.jsx
+++ resources/js/App/User/UserEvents.jsx
@@ -6,11 +6,12 @@ const UserEvents = (props) => {
   const {user, userEvents} = props
   
   const events = userEvents.length === 0 ?
-  <p>no events created by {user.name}</p>
+  <p>No events created by {user.name}</p>
   :
   userEvents.map(event =>(
     <div className="eventItem" key={event.id}>
-      <p className="eventTitle">{event.title}</p>
+      <h3 className="eventTitle">{event.title}</h3>
+      <p className="eventAddress">{event.address}</p>
       <p className="eventDescription">{event.description}</p>
       <EventButtons
       event={event}
@@ -23,7 +24,7 @@ const UserEvents = (props) => {
           <>
           
           <div className="userEvents">
-            <h2>Your active events:</h2>
+            <h2>Your active events</h2>
             {events}
           </div>
           </>
