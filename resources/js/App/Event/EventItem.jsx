16d02b4c42572a5277b4255d110ba09a08bc98a6
Vojtech Vanek <vanevo00@gmail.com>
2019-11-25 13:54:55 +0100

    big detail appears on click in the listevent

---
+++ resources/js/App/Event/EventItem.jsx
@@ -0,0 +1,99 @@
+import React, {useState} from "react";
+import BigDetail from "../Components/BigDetail.jsx";
+
+const EventItem = props => {
+    const [bigDetailOpen, setBigDetailOpen] = useState(null);
+
+    const { event } = props;
+
+    //star rating constants
+    const emptyStar = "far fa-star";
+    const fullStar = "fas fa-star full";
+    const halfStar = "fas fa-star-half-alt full";
+
+    //open big detail on click
+    const handleClick = () => {
+      setBigDetailOpen(event);
+      // setSelectedEvent(null);
+    };
+
+    //logic for adding star ratings
+    const starRating = avgRating => {
+        let remainingRating = avgRating;
+        const stars = [];
+
+        for (let i = 0; i < 5; i += 1) {
+            if (remainingRating > 1) {
+                stars.push(fullStar);
+                remainingRating -= 1;
+            } else if (remainingRating === 0.5) {
+                stars.push(halfStar);
+                remainingRating -= 0.5;
+            } else {
+                stars.push(emptyStar);
+            }
+        }
+
+        return (
+            <div className="star-rating">
+                {stars.map((star, index) => {
+                    return <i className={star} key={index}></i>;
+                })}
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <div
+                className={`event-list-item type-${event.type_id}`}
+                key={event.id}
+            >
+                <div
+                    className={`event-cover event-cover-${event.type_id}`}
+                    onClick={() => handleClick(event.id)}
+                >
+                    <div className="event-cover-title">{event.title}</div>
+                    <div className="event-cover-address">
+                        <i className="fas fa-map-marker-alt"></i>
+                        <div>{event.address}</div>
+                    </div>
+                    <div className="event-timeframe">
+                        <i className="fas fa-clock"></i>
+                        <div>{event.starts_at}</div>
+                        <div>-</div>
+                        <div>{event.ends_at}</div>
+                    </div>
+                </div>
+                <div
+                    className="event-image"
+                    style={{
+                        backgroundImage: `url(${
+                            event.images.length !== 0
+                                ? event.images[0].url
+                                : "https://icon-library.net/images/no-image-available-icon/no-image-available-icon-6.jpg"
+                        })`
+                    }}
+                ></div>
+                <div className="event-title">{event.title}</div>
+                <div className="event-rating">
+                    Rating:{" "}
+                    {event.ratings.length !== 0
+                        ? starRating(Math.round(event.avg_rating * 2) / 2) //round to nearest .5
+                        : "no ratings"}
+                </div>
+                <div className="event-address">{event.address}</div>
+            </div>
+            {bigDetailOpen && (
+                <>
+                    <BigDetail
+                        setBigDetailOpen={setBigDetailOpen}
+                        bigDetailOpen={bigDetailOpen}
+                    />
+                </>
+            )}
+        </>
+    );
+};
+
+export default EventItem;
--- resources/js/App/Event/EventListItems.jsx
+++ resources/js/App/Event/EventListItems.jsx
@@ -1,95 +1,20 @@
 import React, { useEffect } from "react";
 import { Spinner } from "reactstrap";
+import EventItem from "./EventItem.jsx";
 
 const EventListItems = props => {
     const { data } = props;
 
-    //star rating constants
-    const emptyStar =  "far fa-star";
-    const fullStar = "fas fa-star full";
-    const halfStar = "fas fa-star-half-alt full";
-
-    //open big detail on click
-    const handleClick = (eventId) => {
-        console.log(eventId, "clicked");
-    }
-
-    useEffect(() => {
-        // console.log(data);
-    });
-
-   //logic for adding star ratings
-    const starRating = (avgRating) => {
-        let remainingRating = avgRating;
-        const stars = [];
-        
-        for (let i = 0; i < 5; i+= 1) {
-            if (remainingRating > 1) {
-                stars.push(fullStar);
-                remainingRating -= 1;
-            } else if (remainingRating === 0.5) {
-                stars.push(halfStar);
-                remainingRating -= 0.5;
-            } else {
-                stars.push(emptyStar);
-            }
-        }
-        
-        return (
-            <div className="star-rating">
-                {stars.map((star, index) => {
-                    return <i className={star} key={index}></i>;
-                })}
-            </div>
-        );
-    }
 
     return (
         <>
             {data ? (
-                data.data.map(event => {
+                data.data.map((event, index) => {
                     return (
-                        <div
-                            className={`event-list-item type-${event.type_id}`}
-                            key={event.id}
-                        >
-                            <div
-                                className={`event-cover event-cover-${event.type_id}`}
-                                onClick={() => handleClick(event.id)}
-                            >
-                                <div className="event-cover-title">
-                                    {event.title}
-                                </div>
-                                <div className="event-cover-address">
-                                    <i className="fas fa-map-marker-alt"></i>
-                                    <div>{event.address}</div>
-                                </div>
-                                <div className="event-timeframe">
-                                    <i className="fas fa-clock"></i>
-                                    <div>{event.starts_at}</div>
-                                    <div>-</div>
-                                    <div>{event.ends_at}</div>
-                                </div>
-                            </div>
-                            <div
-                                className="event-image"
-                                style={{
-                                    backgroundImage: `url(${
-                                        event.images.length !== 0
-                                            ? event.images[0].url
-                                            : "https://icon-library.net/images/no-image-available-icon/no-image-available-icon-6.jpg"
-                                    })`
-                                }}
-                            ></div>
-                            <div className="event-title">{event.title}</div>
-                            <div className="event-rating">
-                                Rating:{" "}
-                                {event.ratings.length !== 0
-                                    ? starRating(Math.round(event.avg_rating * 2) / 2) //round to nearest .5
-                                    : "no ratings"}
-                            </div>
-                            <div className="event-address">{event.address}</div>
-                        </div>
+                        <EventItem
+                        event={event}
+                        key={index}
+                        />
                     );
                 })
             ) : (
