52400a7f0dbfb9f51586700291b0e33b4592cb0f
Vojtech Vanek <vanevo00@gmail.com>
2019-11-26 16:22:03 +0100

    small changes in commentEvent

--- resources/js/App/Components/CommentEvent.jsx
+++ resources/js/App/Components/CommentEvent.jsx
@@ -58,6 +58,7 @@ const CommentEvent = props => {
                     </div>
                 );
                 commentCallback(data);
+                setComment("");
             }
         }
     }, [data]);
