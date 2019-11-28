6fcb5b01ef02302c927e6f3cbad4b90ee2a74943
JT <jakubtalacko@gmail.com>
2019-11-28 13:20:10 +0100

    openign animation

---
+++ resources/js/App/Components/OpeningAnimationPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const OpeningAnimationPage = () => {
+    return (
+        <div className="animationWrapper">
+            <div className="containerOpeningAnimation">
+                <div className="typedTextContainer">
+                    <h1>happenin'</h1>
+                </div>
+                <div className="animationContainer">
+                    <div className="bar bar1">
+                        <img src="./images/1.png" alt="" />
+                    </div>
+                    <div className="bar bar2">
+                        <img src="./images/2.png" alt="" />
+                    </div>
+                    <div className="bar bar3">
+                        <img src="./images/3.png" alt="" />
+                    </div>
+                    <div className="bar bar4">
+                        <img src="./images/4.png" alt="" />
+                    </div>
+                    <div className="bar bar5">
+                        <img src="./images/5.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default OpeningAnimationPage;
